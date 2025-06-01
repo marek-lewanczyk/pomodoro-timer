1e9cb7e27ca06b2bb0435790f57d1404d27353ec
Marek <marek.lewanczyk@outlook.com>
2025-06-01 18:08:54 +0200

    initialize Pomodoro Timer application with React, TypeScript, and Vite setup

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {RouterProvider} from "react-router";
+import router from "@/router/router.tsx";
+
+const root = document.getElementById('root');
+
+createRoot(root!).render(
+    <RouterProvider router={router} />
+)
