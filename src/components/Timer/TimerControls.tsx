4442e4cab74f158efb35a270d739802676c0bdc3
Marek <marek.lewanczyk@outlook.com>
2025-06-01 20:40:48 +0200

    update TimerControls to use icons for start, pause, and reset buttons

--- src/components/Timer/TimerControls.tsx
+++ src/components/Timer/TimerControls.tsx
@@ -1,4 +1,5 @@
 import type {TimerControlsProps} from "@/types/timer.ts";
+import {PlayPauseIcon, XMarkIcon} from "@heroicons/react/16/solid";
 
 export default function TimerControls({
                                           isRunning,
@@ -13,14 +14,14 @@ export default function TimerControls({
                     onClick={onStart}
                     className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
                 >
-                    Start
+                    <PlayPauseIcon className="h-8 w-8" />
                 </button>
             ) : (
                 <button
                     onClick={onPause}
                     className="px-6 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition"
                 >
-                    Pause
+                    <PlayPauseIcon className="h-8 w-8" />
                 </button>
             )}
 
@@ -28,7 +29,7 @@ export default function TimerControls({
                 onClick={onReset}
                 className="px-6 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition"
             >
-                Reset
+                <XMarkIcon className="h-8 w-8" />
             </button>
         </div>
     )
