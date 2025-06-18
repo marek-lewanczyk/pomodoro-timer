a6985154db103329ac82a49255250d63cb9b7100
Marek <marek.lewanczyk@outlook.com>
2025-06-18 05:51:39 +0200

    bump version to 1.0.0; refactor TaskItem component for improved mobile and desktop layouts

--- src/components/Tasks/TaskItem/TaskItem.tsx
+++ src/components/Tasks/TaskItem/TaskItem.tsx
@@ -23,71 +23,81 @@ function TaskItem({ task, isActive }: TaskItemProps) {
   };
 
   return (
-    <li className="w-full flex flex-col bg-secondary dark:bg-primary">
-      <div className="font-vt323 flex justify-between items-center gap-5">
-        <div className="w-full flex items-center gap-3">
-          <Checkbox
-            checked={task.isCompleted}
-            onChange={() => toggleComplete(task.id)}
-          />
-          {isEditing ? (
-            <TextareaField
-              name="editedTitle"
-              value={editedTitle}
-              onChange={(e) => setEditedTitle(e.target.value)}
-              className="p-1 w-auto font-vt323 text-sm bg-secondary border border-primary shadow focus:outline-none dark:bg-primary dark:border-secondary dark:shadow-secondary"
+      <li className="w-full flex flex-col bg-secondary dark:bg-primary">
+        <div className="font-vt323 flex justify-between items-center gap-5">
+          <div className="w-full flex items-center gap-3">
+            <Checkbox
+                checked={task.isCompleted}
+                onChange={() => toggleComplete(task.id)}
             />
-          ) : (
-            <span
-              className={`flex-1 text-sm text-justify ${
-                task.isCompleted ? "line-through" : ""
-              }`}
-            >
+            {isEditing ? (
+                <TextareaField
+                    name="editedTitle"
+                    value={editedTitle}
+                    onChange={(e) => setEditedTitle(e.target.value)}
+                    className="p-1 w-auto font-vt323 text-sm bg-secondary border border-primary shadow focus:outline-none dark:bg-primary dark:border-secondary dark:shadow-secondary"
+                />
+            ) : (
+                <span
+                    className={`flex-1 text-sm text-justify ${
+                        task.isCompleted ? "line-through" : ""
+                    }`}
+                >
               {task.title}
             </span>
-          )}
-        </div>
-
-        <div className="flex items-center gap-4">
-          <TaskPomodoroCount count={task.pomodoroCount} isActive={isActive} />
-          <Button
-            onClick={() => setIsExpanded((prev) => !prev)}
-            className="lg:hidden"
-          >
-            {isExpanded ? (
-              <ChevronDownIcon className="h-8 w-8" />
-            ) : (
-              <ChevronUpIcon className="h-8 w-8" />
             )}
-          </Button>
-        </div>
-      </div>
+          </div>
 
-      <div
-        className={`mt-2 gap-4 flex-col lg:flex-row lg:items-center lg:justify-end ${
-          isExpanded ? "flex flex-row items-center justify-end" : "hidden"
-        } lg:flex`}
-      >
-        <TaskActions
-          isCompleted={task.isCompleted}
-          isActive={isActive}
-          isEditing={isEditing}
-          onSetActive={() => setActiveTask(task.id)}
-          onEdit={handleEdit}
-          onDelete={() => deleteTask(task.id)}
-        />
-      </div>
-    </li>
+          <div className="flex items-center gap-4">
+            <TaskPomodoroCount count={task.pomodoroCount} isActive={isActive} />
+            <div className="lg:hidden">
+              <Button onClick={() => setIsExpanded((prev) => !prev)}>
+                {isExpanded ? (
+                    <ChevronDownIcon className="h-8 w-8" />
+                ) : (
+                    <ChevronUpIcon className="h-8 w-8" />
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile-only options */}
+        {isExpanded && (
+            <div className="mt-2 gap-4 flex flex-col items-end lg:hidden">
+              <TaskActions
+                  isCompleted={task.isCompleted}
+                  isActive={isActive}
+                  isEditing={isEditing}
+                  onSetActive={() => setActiveTask(task.id)}
+                  onEdit={handleEdit}
+                  onDelete={() => deleteTask(task.id)}
+              />
+            </div>
+        )}
+
+        {/* Desktop-only options (always visible) */}
+        <div className="mt-2 gap-4 hidden lg:flex lg:flex-row lg:items-center lg:justify-end">
+          <TaskActions
+              isCompleted={task.isCompleted}
+              isActive={isActive}
+              isEditing={isEditing}
+              onSetActive={() => setActiveTask(task.id)}
+              onEdit={handleEdit}
+              onDelete={() => deleteTask(task.id)}
+          />
+        </div>
+      </li>
   );
 }
 
 function areEqual(prev: TaskItemProps, next: TaskItemProps) {
   return (
-    prev.task.id === next.task.id &&
-    prev.task.title === next.task.title &&
-    prev.task.isCompleted === next.task.isCompleted &&
-    prev.task.pomodoroCount === next.task.pomodoroCount &&
-    prev.isActive === next.isActive
+      prev.task.id === next.task.id &&
+      prev.task.title === next.task.title &&
+      prev.task.isCompleted === next.task.isCompleted &&
+      prev.task.pomodoroCount === next.task.pomodoroCount &&
+      prev.isActive === next.isActive
   );
 }
 
